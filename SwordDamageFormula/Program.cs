ed349b3b97efbd0ed8446a2afb47f6e27064c8b0
yasthir <yasthir01@gmail.com>
2022-04-11 22:23:51 +0200

    Add project files.

---
+++ SwordDamageFormula/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SwordDamageFormula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwordDamage swordDamage = new SwordDamage();
+            Random random = new Random();
+
+            while (true)
+            {
+                Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, " +
+                    "3 for both, anything else to quit: ");
+                char key = Console.ReadKey().KeyChar;
+                // if the key is NOT 0, 1, 2 or 3... then return to end program
+                if (key != '0' && key != '1' && key != 2 && key != '3') return;
+                // roll the dice 3 times and add the results together
+                int roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+                swordDamage.Roll = roll;
+                // if user presses 1 or 3 then set the magic
+                swordDamage.SetMagic(key == '1' || key == '3');
+                // if the user pressed 2 or 3 then set flaming
+                swordDamage.SetFlaming(key == '2' || key == '3');
+                Console.WriteLine("\nRolled " + roll + " for " + swordDamage.Damage + " HP\n");
+            }
+        }
+    }
+}
---
+++ SwordDamageFormula/SwordDamage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwordDamageFormula
+{
+    class SwordDamage
+    {
+        // create constants for the BASE_DAMAGE and FLAME_DAMAGE
+        public const int BASE_DAMAGE = 3;  // 3HP points
+        public const int FLAME_DAMAGE = 2;  // 2HP points
+
+        public int Roll;
+        public decimal MagicMultiplier = 1M;
+        public int FlamingDamage = 0;
+        public int Damage;
+
+        public void CalculateDamage()
+        {
+            // calculate the damage formula
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+            CalculateDamage();
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            // flaming swords cause extra damage in addition to the roll.
+            // so the method will have to calculate damage and then add FLAME_DAMAGE to it
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+        }
+    }
+}
